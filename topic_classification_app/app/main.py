641c776773bd48858b2e8cca708b3113ae6b2694
Andreas Gompos <andreas.gompos@gmail.com>
2018-07-25 21:09:50 +0100

    replaced flask with sanic

--- topic_classification_app/app/main.py
+++ topic_classification_app/app/main.py
@@ -1,30 +1,32 @@
-from flask import Flask, request, render_template, jsonify
+from sanic import Sanic, request
+from sanic.response import json
 from dependencies import *
 
-app = Flask(__name__)
-
-preprocessing_pipeline = joblib.load('./artifacts/preprocessing_pipeline.pkl') 
-model = load_model("./artifacts/model")
-
-test_processed_text = preprocessing_pipeline.transform('testing')
-test_probas = model.predict_proba(test_processed_text, verbose=0) * 100
+app = Sanic()
 
 @app.route("/", methods=['POST'])
-def predict():
+async def predict(request):
 
-    data = request.get_json()
-    processed_text = preprocessing_pipeline.transform(data['text'])
+    incoming_request = request.json
+    processed_text = preprocessing_pipeline.transform(incoming_request['text'])
     probas = model.predict_proba(processed_text, verbose=0) * 100
     
-    result = {'business': str(probas[0][0]),
-              'entertainment': str(probas[0][1]),
-              'politics': str(probas[0][2]),
-              'sport': str(probas[0][3]),
-              'tech': str(probas[0][4])}
+    model_response = {'business': str(probas[0][0]),
+                      'entertainment': str(probas[0][1]),
+                      'politics': str(probas[0][2]),
+                      'sport': str(probas[0][3]),
+                      'tech': str(probas[0][4])}
 
-    return jsonify(result)
-
+    return json(model_response)
 
 if __name__ == "__main__":
 
-    app.run(host='0.0.0.0', port = 5000)
+    print("loading models - waiting until the server is up")
+    preprocessing_pipeline = joblib.load('./artifacts/preprocessing_pipeline.pkl') 
+    model = load_model("./artifacts/model")
+
+    test_processed_text = preprocessing_pipeline.transform('testing')
+    test_probas = model.predict_proba(test_processed_text, verbose=0) * 100
+    print('server is up')
+
+    app.run(host="0.0.0.0", port=5000, workers=1)
